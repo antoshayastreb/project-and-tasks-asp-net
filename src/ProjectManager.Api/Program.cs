987f1aba38d44f9d8c035c6cbd61443403f9de02
antoshayastreb <nedanon@outlook.com>
2026-04-22 21:25:29 +0300

    feat: serilog settings.

--- src/ProjectManager.Api/Program.cs
+++ src/ProjectManager.Api/Program.cs
@@ -9,51 +9,61 @@ using ProjectManager.Infrastructure.Persistence.Repositories;
 using ProjectManager.Infrastructure.Queries;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger  = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Host.UseSerilog((context, services, configuration) => configuration
-    .ReadFrom.Configuration(context.Configuration)
-    .ReadFrom.Services(services)
-    .Enrich.FromLogContext()                  
-    .WriteTo.Console());
-
-builder.Services.AddControllers(); 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-builder.Services.AddScoped<IProjectTaskRepository, ProjectTaskRepository>();
-builder.Services.AddScoped<IProjectQueries, ProjectQueries>();
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IProjectTaskService, ProjectTaskService>();
-builder.Services.AddScoped<IProjectTaskQueries, ProjectTaskQueries>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-builder.Services.AddValidatorsFromAssemblyContaining<CreateProjectDto>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services));
+
+    builder.Services.AddControllers(); 
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+    builder.Services.AddScoped<IProjectTaskRepository, ProjectTaskRepository>();
+    builder.Services.AddScoped<IProjectQueries, ProjectQueries>();
+    builder.Services.AddScoped<IProjectService, ProjectService>();
+    builder.Services.AddScoped<IProjectTaskService, ProjectTaskService>();
+    builder.Services.AddScoped<IProjectTaskQueries, ProjectTaskQueries>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+    builder.Services.AddValidatorsFromAssemblyContaining<CreateProjectDto>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseSerilogRequestLogging(); 
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+    app.Run();
 }
-
-app.UseSerilogRequestLogging(); 
-app.UseHttpsRedirection();
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
+catch (Exception exc)
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await dbContext.Database.MigrateAsync();
+    Log.Fatal(exc, "Unexpected error");
 }
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
